684dc0cc4ad4c70cd6423e4421d1df97e5e5e8df
festchellin <dreamerhaozhe@outlook.com>
2018-09-17 10:13:04 +0800

    init repository

--- src/main/java/com/zhibo/org/zhibo/controller/TestController.java
+++ src/main/java/com/zhibo/org/zhibo/controller/TestController.java
@@ -3,17 +3,19 @@ package com.zhibo.org.zhibo.controller;
 import com.zhibo.org.zhibo.entity.User;
 import com.zhibo.org.zhibo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
     @Autowired
     private UserMapper userMapper;
 
     @RequestMapping("/")
-    public Object getUserById(){
+    public String getUserById(Model model){
         User user = userMapper.getUserById(1);
-        return user;
+        model.addAttribute("user",user);
+        return "/index";
     }
 }
